ad07618958058983085c331bf50b5e68a71dd242
Gabriel Brammer <brammer@stsci.edu>
2011-05-18 05:37:27 +0000

    Initial setup of __init__ and prepare.
    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@2 f9184c78-529c-4a83-b317-4cf1064cc5e0

---
+++ __init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Gabriel Brammer on 2011-05-18.
+
+$URL$
+$Author$
+$Date$
+
+"""
+__version__ = "$Rev$"
+
+import os
+import glob
+import shutil
+
+import numpy as np
+import matplotlib.pyplot as plt
+USE_PLOT_GUI = False
+
+import pyfits
+
+import threedhst
+
+import prepare
+
---
+++ prepare.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+prepare.py
+
+Created by Gabriel Brammer on 2011-05-18.
+
+$URL$
+$Author$
+$Date$
+
+"""
+__version__ = "$Rev$"
+
+import sys
+import os
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+

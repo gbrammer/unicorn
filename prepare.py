7d5296b6838fb0fa3fb351a0617912fd2ab8714b
Gabriel Brammer <brammer@stsci.edu>
2011-05-18 05:53:08 +0000

    Starting with COSMOS...
    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@3 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- prepare.py
+++ prepare.py
@@ -3,23 +3,39 @@
 """
 prepare.py
 
-Created by Gabriel Brammer on 2011-05-18.
+Scripts to "prepare" the FLT files for the 3D-HST reduction:
 
-$URL$
-$Author$
-$Date$
+Direct images:
+    1) Align to WCS reference
+    2) Subtract sky backgrounds
+    3) Run multidrizzle to flag cosmic rays
+    4) Make mosaics combining pointings for a given field
+    
+Grism images:
+    1) Copy shifts from direct images
+    2) Divide by the F140W flat
+    3) Subtract sky background
+    4) Run multidrizzle to flag cosmic rays
+    5) ...
+    
+$ URL: https://subversion.assembla.com/svn/threedhst_internal/trunk/prepare.py $
+$ Author: gbrammer $
+$ Date: 2011-05-18 00:37:27 -0500 (Wed, 18 May 2011) $
 
 """
-__version__ = "$Rev$"
+__version__ = " $ Rev: 2 $"
 
-import sys
-import os
+def COSMOS():
+    from threedhst.prep_flt_files import process_3dhst_pair as pair
+    import threedhst.prep_flt_files
 
-
-def main():
-	pass
-
-
-if __name__ == '__main__':
-	main()
-
+    os.chdir('/3DHST/Spectra/Work/COSMOS/PREP_FLT')
+    ALIGN = '/3DHST/Ancillary/COSMOS/WIRDS/WIRDS_Ks_100028+021230_T0002.fits'
+    
+    #### Direct images only
+    direct=glob.glob('*30_asn.fits')
+    gris = glob.glob('*40_asn.fits')
+    for i in range(len(direct)):
+        pair(direct[i], grism[i], ALIGN_IMAGE = ALIGN, SKIP_GRISM=True)
+    
+    

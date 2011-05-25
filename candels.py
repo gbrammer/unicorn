b76fcd03ee42676a6be444f1f9016af66462156d
Gabriel Brammer <brammer@stsci.edu>
2011-05-25 22:56:31 +0000

    Add CANDELS scripts.
    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@8 f9184c78-529c-4a83-b317-4cf1064cc5e0

---
+++ candels.py
@@ -0,0 +1,393 @@
+import os
+import glob
+import shutil
+
+import numpy as np
+import pyfits
+import scipy.linalg
+# from scipy import polyfit, polyval
+import matplotlib.pyplot as plt
+
+from pyraf import iraf
+from iraf import stsdas,dither,slitless,axe 
+
+INDEF = iraf.INDEF
+no = iraf.no
+yes = iraf.yes
+
+import threedhst
+import threedhst.prep_flt_files
+import threedhst.catIO as catIO
+
+def egs():
+    import unicorn.candels
+    
+    unicorn.candels.make_asn_files()
+    
+    ALIGN_IMAGE = 'AEGIS-N2_K_sci.fits'
+    ALIGN_IMAGE = '../WIRDS/WIRDS_Ks_141927+524056_T0002.SUB2.fits'
+    
+    files=glob.glob('EGS-V[567EF]*asn.fits')
+    for file in files:
+        if not os.path.exists(file.replace('asn','drz')):
+            unicorn.candels.prep_candels(asn_file=file, 
+                ALIGN_IMAGE = ALIGN_IMAGE, ALIGN_EXTENSION=0,
+                GET_SHIFT=True, DIRECT_HIGHER_ORDER=2)
+    #
+    
+    for filt in ['F125W', 'F160W']:
+        files=glob.glob('EGS-V[567EF]*-'+filt+'_asn.fits')
+        threedhst.utils.combine_asn_shifts(files, out_root='EGS-epoch1-'+filt,
+                       path_to_FLT='./', run_multidrizzle=False)
+        #
+        SCALE = 0.06
+        threedhst.prep_flt_files.startMultidrizzle( 'EGS-epoch1-'+filt+'_asn.fits',
+             use_shiftfile=True, skysub=False,
+             final_scale=SCALE, pixfrac=0.8, driz_cr=False,
+             updatewcs=False, clean=True, median=False,
+             ra=214.92061, dec=52.878457, final_outnx=7547, 
+             final_outny=31900, final_rot=42)
+
+def uds():
+    import unicorn.candels
+    
+    #unicorn.candels.make_asn_files()
+    
+    list = catIO.Readfile('files.info')
+    epoch1 = list.date_obs < '2010-12-15'
+    epoch2 = list.date_obs > '2010-12-15'
+    
+    ALIGN_IMAGE='../UKIDSS/UDS_K.fits'
+    
+    ########################
+    ### Epoch1
+    ########################
+    visits = np.unique(list.targname[epoch1])
+    for visit in visits:
+        filters = np.unique(list.filter[list.targname == visit])
+        for filter in filters:
+            file = visit+'-'+filter+'_asn.fits'
+            if not os.path.exists(file.replace('asn','drz')):
+                unicorn.candels.prep_candels(asn_file=file, 
+                    ALIGN_IMAGE = ALIGN_IMAGE, ALIGN_EXTENSION=0,
+                    GET_SHIFT=True, DIRECT_HIGHER_ORDER=2,
+                    SCALE=0.128254)
+    ## Mosaic
+    f125w_list = []
+    f160w_list = []
+    for visit in visits:
+        f125w_list.append(visit+'-F125W_asn.fits')
+        f160w_list.append(visit+'-F160W_asn.fits')
+    
+    threedhst.utils.combine_asn_shifts(f125w_list,out_root='UDS-epoch1-F125W', path_to_FLT='./', run_multidrizzle=False)
+    threedhst.utils.combine_asn_shifts(f160w_list,out_root='UDS-epoch1-F160W', path_to_FLT='./', run_multidrizzle=False)
+    
+    for filt in ['F125W', 'F160W']:
+        SCALE = 0.128254
+        ra, dec, nx, ny = 34.4081405556, -5.20018201235, 10422, 4349
+        threedhst.prep_flt_files.startMultidrizzle( 'UDS-epoch1-'+filt+'_asn.fits',
+             use_shiftfile=True, skysub=False,
+             final_scale=SCALE, pixfrac=1.0, driz_cr=False,
+             updatewcs=False, clean=True, median=False,
+             ra=ra, dec=dec, final_outnx=nx, final_outny=ny)
+             
+    ########################
+    ### Epoch2
+    ########################
+    visits = np.unique(list.targname[epoch2])
+    for visit in visits:
+        filters = np.unique(list.filter[list.targname == visit])
+        for filter in filters:
+            file = visit+'-'+filter+'_asn.fits'
+            if not os.path.exists(file.replace('asn','drz')):
+                unicorn.candels.prep_candels(asn_file=file, 
+                    ALIGN_IMAGE = ALIGN_IMAGE, ALIGN_EXTENSION=0,
+                    GET_SHIFT=True, DIRECT_HIGHER_ORDER=2,
+                    SCALE=0.128254)
+    ## Mosaic
+    f125w_list = []
+    f160w_list = []
+    for visit in visits:
+        f125w_list.append(visit+'-F125W_asn.fits')
+        f160w_list.append(visit+'-F160W_asn.fits')
+    
+    threedhst.utils.combine_asn_shifts(f125w_list,out_root='UDS-epoch2-F125W', path_to_FLT='./', run_multidrizzle=False)
+    threedhst.utils.combine_asn_shifts(f160w_list,out_root='UDS-epoch2-F160W', path_to_FLT='./', run_multidrizzle=False)
+    
+    for filt in ['F125W', 'F160W']:
+        SCALE = 0.128254
+        ra, dec, nx, ny = 34.4081405556, -5.20018201235, 10422, 4349
+        threedhst.prep_flt_files.startMultidrizzle( 'UDS-epoch2-'+filt+'_asn.fits',
+             use_shiftfile=True, skysub=False,
+             final_scale=SCALE, pixfrac=1.0, driz_cr=False,
+             updatewcs=False, clean=True, median=False,
+             ra=ra, dec=dec, final_outnx=nx, final_outny=ny)
+
+    ########################
+    ### Full Mosaic
+    ########################
+    f125w_list = glob.glob('UDS-V*-F125W_asn.fits')
+    f160w_list = glob.glob('UDS-V*-F160W_asn.fits')
+    threedhst.utils.combine_asn_shifts(f125w_list,out_root='UDS-F125W', path_to_FLT='./', run_multidrizzle=False)
+    threedhst.utils.combine_asn_shifts(f160w_list,out_root='UDS-F160W', path_to_FLT='./', run_multidrizzle=False)
+    
+    for filt in ['F125W', 'F160W']:
+        SCALE = 0.128254
+        ra, dec, nx, ny = 34.4081405556, -5.20018201235, 10422, 4349
+        threedhst.prep_flt_files.startMultidrizzle( 'UDS-'+filt+'_asn.fits',
+             use_shiftfile=True, skysub=False,
+             final_scale=SCALE, pixfrac=1.0, driz_cr=False,
+             updatewcs=False, clean=True, median=False,
+             ra=ra, dec=dec, final_outnx=nx, final_outny=ny)
+
+def cdfs():
+    import unicorn.candels
+
+    unicorn.candels.make_asn_files()
+    
+    list = catIO.Readfile('files.info')
+    
+    list.group = []
+    for targ in list.targname:
+        list.group.append('-'.join(targ.split('-')[0:2]))
+    
+    list.group = np.array(list.group)
+    
+    epoch1 = list.group == 'GOODS-SD5'
+    epoch2 = list.group == 'GOODS-SD2'
+    epoch3 = list.group == 'GOODS-S075'
+    epoch4 = list.group == 'GOODS-SDW'
+
+    wide1 = list.group == 'GOODS-W1'
+    wide2 = list.group == 'GOODS-W2'
+
+    ALIGN_IMAGE='../ACS/h_sz*drz*fits'
+    #ALIGN_IMAGE='../PREP_FLT/goods-s_f160w_drz.fits'
+    
+    ########################
+    ### Deep epochs
+    ########################
+    epochs = [epoch1, epoch2, epoch3, epoch4]
+    
+    for i,epoch in enumerate(epochs[1:]):
+        e=i+2
+        #e=i+1
+        visits = np.unique(list.targname[epoch])
+        for visit in visits:
+            filters = np.unique(list.filter[list.targname == visit])
+            for filter in filters:
+                file = visit+'-'+filter+'_asn.fits'
+                if not os.path.exists(file.replace('asn','drz')):
+                    unicorn.candels.prep_candels(asn_file=file, 
+                        ALIGN_IMAGE = ALIGN_IMAGE, ALIGN_EXTENSION=0,
+                        GET_SHIFT=False, DIRECT_HIGHER_ORDER=2,
+                        SCALE=0.128254)
+        ## Mosaic
+        f125w_list = []
+        f160w_list = []
+        for visit in visits:
+            f125w_list.append(visit+'-F125W_asn.fits')
+            f160w_list.append(visit+'-F160W_asn.fits')
+        #
+        threedhst.utils.combine_asn_shifts(f125w_list,out_root='GOODS-epoch%d-F125W' %(e), path_to_FLT='./', run_multidrizzle=False)
+        threedhst.utils.combine_asn_shifts(f160w_list,out_root='GOODS-epoch%d-F160W' %(e), path_to_FLT='./', run_multidrizzle=False)
+        #
+        SCALE = 0.128254
+        for filt in ['F125W', 'F160W']:
+            ra, dec, nx, ny = 53.127353, -27.798616, int(5670*0.128254/SCALE), int(5220*0.128254/SCALE)
+            threedhst.prep_flt_files.startMultidrizzle( 'GOODS-epoch%d-' %(e)+filt+'_asn.fits',
+                use_shiftfile=True, skysub=False,
+                final_scale=SCALE, pixfrac=1.0, driz_cr=False,
+                updatewcs=False, clean=True, median=False,
+                ra=ra, dec=dec, final_outnx=nx, final_outny=ny)
+    #
+    files=glob.glob('GOODS-S*drz.fits')
+    files.extend(glob.glob('*align.fits'))
+    for file in files: 
+        os.remove(file)
+        
+    #### Full deep mosaic
+    f125w_list = glob.glob('GOODS-S*F125W_asn.fits')
+    f160w_list = glob.glob('GOODS-S*F160W_asn.fits')
+    threedhst.utils.combine_asn_shifts(f125w_list,out_root='GOODS-deep-F125W', path_to_FLT='./', run_multidrizzle=False)
+    threedhst.utils.combine_asn_shifts(f160w_list,out_root='GOODS-deep-F160W', path_to_FLT='./', run_multidrizzle=False)
+
+    SCALE = 0.128254
+    ra, dec, nx, ny = 53.127353, -27.798616, int(5670*0.128254/SCALE), int(5220*0.128254/SCALE)
+    for filt in ['F125W', 'F160W']:
+        threedhst.prep_flt_files.startMultidrizzle('GOODS-deep-'+filt+ '_asn.fits',
+        use_shiftfile=True, skysub=False,
+        final_scale=SCALE, pixfrac=1.0, driz_cr=False,
+        updatewcs=False, clean=True, median=False,
+        ra=ra, dec=dec, final_outnx=nx, final_outny=ny)
+    
+    ########################
+    ### Wide epochs
+    ########################
+    ALIGN_IMAGE = '../ECDFS/MUSYC_ECDFS_BVR.fits'
+    epochs = [wide1, wide2]
+    
+    for i,epoch in enumerate(epochs):
+        e=i+1
+        visits = np.unique(list.targname[epoch])
+        for visit in visits:
+            filters = np.unique(list.filter[list.targname == visit])
+            for filter in filters:
+                file = visit+'-'+filter+'_asn.fits'
+                if not os.path.exists(file.replace('asn','drz')):
+                    unicorn.candels.prep_candels(asn_file=file, 
+                        ALIGN_IMAGE = ALIGN_IMAGE, ALIGN_EXTENSION=0,
+                        GET_SHIFT=True, DIRECT_HIGHER_ORDER=2,
+                        SCALE=0.128254)
+        ## Mosaic
+        f125w_list = []
+        f160w_list = []
+        for visit in visits:
+            f125w_list.append(visit+'-F125W_asn.fits')
+            f160w_list.append(visit+'-F160W_asn.fits')
+        #
+        threedhst.utils.combine_asn_shifts(f125w_list,out_root='GOODS-wide%d-F125W' %(e), path_to_FLT='./', run_multidrizzle=False)
+        threedhst.utils.combine_asn_shifts(f160w_list,out_root='GOODS-wide%d-F160W' %(e), path_to_FLT='./', run_multidrizzle=False)
+        #
+        SCALE = 0.128254
+        for filt in ['F125W', 'F160W']:
+            ra, dec, nx, ny = 53.165233, -27.900847, int(5350*0.128254/SCALE), int(3680*0.128254/SCALE)
+            threedhst.prep_flt_files.startMultidrizzle( 'GOODS-wide%d-' %(e)+filt+'_asn.fits',
+                use_shiftfile=True, skysub=False,
+                final_scale=SCALE, pixfrac=1.0, driz_cr=False,
+                updatewcs=False, clean=True, median=False,
+                ra=ra, dec=dec, final_outnx=nx, final_outny=ny)
+    #
+    files=glob.glob('GOODS-W*drz.fits')
+    files.extend(glob.glob('*align.fits'))
+    for file in files: 
+        os.remove(file)
+    
+    #### Full wide mosaic
+    f125w_list = glob.glob('GOODS-W?-*F125W_asn.fits')
+    f160w_list = glob.glob('GOODS-W?-*F160W_asn.fits')
+    threedhst.utils.combine_asn_shifts(f125w_list,out_root='GOODS-wide-F125W', path_to_FLT='./', run_multidrizzle=False)
+    threedhst.utils.combine_asn_shifts(f160w_list,out_root='GOODS-wide-F160W', path_to_FLT='./', run_multidrizzle=False)
+
+    SCALE = 0.128254
+    ra, dec, nx, ny = 53.165233, -27.900847, int(5670*0.128254/SCALE), int(5220*0.128254/SCALE)
+    for filt in ['F125W', 'F160W']:
+        threedhst.prep_flt_files.startMultidrizzle('GOODS-wide-'+filt+ '_asn.fits',
+        use_shiftfile=True, skysub=False,
+        final_scale=SCALE, pixfrac=1.0, driz_cr=False,
+        updatewcs=False, clean=True, median=False,
+        ra=ra, dec=dec, final_outnx=nx, final_outny=ny)
+    
+    ########################
+    ###   ERS imaging
+    ########################
+    ALIGN_IMAGE='../ACS/h_sz*drz*fits'
+    ers = list.group == 'WFC3-ERSII'
+    
+    visits = np.unique(list.targname[ers])
+    for visit in visits:
+        filters = np.unique(list.filter[list.targname == visit])
+        for filter in filters:
+            if filter.startswith('G'):
+                continue
+            file = visit+'-'+filter+'_asn.fits'
+            if not os.path.exists(file.replace('asn','drz')):
+                unicorn.candels.prep_candels(asn_file=file, 
+                    ALIGN_IMAGE = ALIGN_IMAGE, ALIGN_EXTENSION=0,
+                    GET_SHIFT=True, DIRECT_HIGHER_ORDER=2,
+                    SCALE=0.128254)
+    ## Mosaic
+    f098m_list = []
+    f125w_list = []
+    f160w_list = []
+    for visit in visits:
+        if not 'G01' in visit:
+            f098m_list.append(visit+'-F098M_asn.fits')
+            f125w_list.append(visit+'-F125W_asn.fits')
+            f160w_list.append(visit+'-F160W_asn.fits')
+    #
+    threedhst.utils.combine_asn_shifts(f098m_list,out_root='GOODS-ERS-F098M', path_to_FLT='./', run_multidrizzle=False)
+    threedhst.utils.combine_asn_shifts(f125w_list,out_root='GOODS-ERS-F125W', path_to_FLT='./', run_multidrizzle=False)
+    threedhst.utils.combine_asn_shifts(f160w_list,out_root='GOODS-ERS-F160W', path_to_FLT='./', run_multidrizzle=False)
+    #
+    SCALE = 0.128254
+    for filt in ['F098M','F125W', 'F160W']:
+        ra, dec, nx, ny = 53.098662, -27.715892, int(5410*0.128254/SCALE), int(3460*0.128254/SCALE)
+        threedhst.prep_flt_files.startMultidrizzle( 'GOODS-ERS-'+filt+ '_asn.fits',
+            use_shiftfile=True, skysub=False,
+            final_scale=SCALE, pixfrac=1.0, driz_cr=False,
+            updatewcs=False, clean=True, median=False,
+            ra=ra, dec=dec, final_outnx=nx, final_outny=ny)
+    #
+    files=glob.glob('WFC3-ERSII-IR*drz.fits')
+    files.extend(glob.glob('*align.fits'))
+    for file in files: 
+        os.remove(file)
+            
+def make_asn_files(force=False):
+    """
+    Read a files.info file and make ASN files for each visit/filter.
+    """
+    list = catIO.Readfile('files.info')
+    asn = threedhst.utils.ASNFile(glob.glob('../RAW/i*asn.fits')[1])
+    
+    visits = np.unique(list.targname)
+    for visit in visits:        
+        filters = np.unique(list.filter[list.targname == visit])
+        print visit, filters
+        for filter in filters:
+            if (os.path.exists(visit+'-'+filter+'_asn.fits')) & (not force):
+                continue
+            
+            #print asn.in_fits[1].columns
+            
+            use = (list.targname == visit) & (list.filter == filter)
+            asn.exposures = []
+            asn.product=visit+'-'+filter
+            for fits in list.file[use]:
+                asn.exposures.append(os.path.basename(fits).split('_flt')[0])
+            
+            #try:
+            asn.write(asn.product+'_asn.fits')
+            #except:
+            #    continue
+                
+            threedhst.regions.asn_region(asn.product+'_asn.fits', path_to_flt='../RAW')
+            print asn.product
+            
+    
+def prep_candels(asn_file='ib3706050_asn.fits',
+                       ALIGN_IMAGE='../ACS/h_nz_sect*img.fits',
+                       ALIGN_EXTENSION=0,
+                       PATH_TO_RAW='../RAW',
+                       GET_SHIFT=True,
+                       DIRECT_HIGHER_ORDER=2,
+                       SCALE=0.06):
+    
+    import threedhst
+    import threedhst.prep_flt_files
+    
+    threedhst.process_grism.fresh_flt_files(asn_file, from_path=PATH_TO_RAW)
+
+    ##### Make region files for the pointing
+    if not os.path.exists(asn_file.replace('fits','pointing.reg')):
+        threedhst.regions.asn_region(asn_file)
+    
+    threedhst.prep_flt_files.prep_flt(asn_file=asn_file,
+                    get_shift=GET_SHIFT, 
+                    bg_only=False, bg_skip=False, redo_background=True,
+                    ALIGN_IMAGE=ALIGN_IMAGE, 
+                    ALIGN_EXT=ALIGN_EXTENSION,
+                    skip_drz=False, final_scale=SCALE, pixfrac=0.8,
+                    IMAGES=[],
+                    align_geometry='rxyscale,shift', clean=True,
+                    initial_order=0, save_fit=False)
+    
+    if DIRECT_HIGHER_ORDER > 0:
+        threedhst.prep_flt_files.prep_flt(asn_file=asn_file,
+                    get_shift=False, 
+                    bg_only=False, bg_skip=False, redo_background=False,
+                    skip_drz=False, final_scale=SCALE, pixfrac=0.8,
+                    IMAGES=[], clean=True,
+                    initial_order=DIRECT_HIGHER_ORDER, save_fit=False)
+    

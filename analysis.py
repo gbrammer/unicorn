1a88b3687205ec07b06262f1a870edbffc8a52ef
Gabriel Brammer <brammer@stsci.edu>
2011-07-05 03:37:47 +0000

    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@96 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- analysis.py
+++ analysis.py
@@ -2187,11 +2187,11 @@ def run_eazy_fit(root='COSMOS-23-G141', id=39, OLD_RES = 'FILTER.RES.v8.R300', O
             #### check normalization
             new_norm = np.sum(obs_sed[is_spec]*fobs[is_spec]/efobs[is_spec]**2)/np.sum(obs_sed[is_spec]**2/efobs[is_spec]**2)
             
-            #### Show the results
-            try:
-                os.system('head -2 OUTPUT/%s_%05d.zout |tail -1' %(root, id))
-            except:
-                pass
+        #### Show the results
+        try:
+            os.system('head -2 OUTPUT/%s_%05d.zout |tail -1' %(root, id))
+        except:
+            pass
                 
     lambdaz, temp_sed, lci, obs_sed, fobs, efobs = \
         eazy.getEazySED(0, MAIN_OUTPUT_FILE='%s_%05d' %(root, id), \

ba48f34c74fa8205f8328cef42cfaf0a2c6aae93
Gabriel Brammer <brammer@stsci.edu>
2011-07-27 16:07:58 +0000

    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@210 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- analysis.py
+++ analysis.py
@@ -478,7 +478,7 @@ specphot(id)
         if drMatch > 1:
             return False
         else:
-            return lam, ffix*anorm, np.sqrt(ferr**2+(0.5*spec.field('CONTAM'))**2)*anorm, lci, fobs, efobs, photom_idx
+            return lam, ffix*anorm, np.sqrt(ferr**2+(1.0*spec.field('CONTAM'))**2)*anorm, lci, fobs, efobs, photom_idx
          
     if Verbose:
         print 'Start plot'
@@ -1974,7 +1974,7 @@ def run_eazy_on_all_objects():
                 object = '%s_%05d' %(pointing, id)
                 if (object+'\n' not in log_lines) & (os.path.exists(unicorn.GRISM_HOME+field+'/HTML/ascii/'+object+'.dat')):
                     try:
-                        unicorn.analysis.run_eazy_fit(root=pointing, id=id, compress=0.75, zmin=0.02, zmax=4, TILT_ORDER=1)
+                        unicorn.analysis.run_eazy_fit(root=pointing, id=id, compress=0.75, zmin=0.02, zmax=4, TILT_ORDER=1, pipe=' > log3')
                     except:
                         pass
                     #    
@@ -2039,7 +2039,7 @@ def make_eazy_inputs(root='COSMOS-23-G141', id=39, OLD_RES = 'FILTER.RES.v8.R300
     else:
         #### No match
         sp = threedhst.catIO.Readfile('HTML/ascii/%s_%05d.dat' %(root, id))
-        lam, spflux, sperr = sp.lam, (sp.flux-sp.contam)/1.e-17*(sp.lam/5500.)**2, np.sqrt(sp.error**2+(0.5*sp.contam)**2)/1.e-17*(sp.lam/5500.)**2
+        lam, spflux, sperr = sp.lam, (sp.flux-sp.contam)/1.e-17*(sp.lam/5500.)**2, np.sqrt(sp.error**2+(1.0*sp.contam)**2)/1.e-17*(sp.lam/5500.)**2
         z_spec = -1.0
         fobs = fobs*0-999
         efobs = fobs

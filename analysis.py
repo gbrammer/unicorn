e6672c7a5ee40dd7d771825259afa7ce6e927437
Gabriel Brammer <brammer@stsci.edu>
2011-07-28 02:21:41 +0000

    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@238 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- analysis.py
+++ analysis.py
@@ -2361,14 +2361,15 @@ def run_eazy_fit(root='COSMOS-23-G141', id=39, OLD_RES = 'FILTER.RES.v8.R300', O
             status = os.system(eazy_binary + ' -p '+'%s_%05d' %(root, id)+'.eazy.param '+pipe)
         
             ztmp = catIO.Readfile('OUTPUT/%s_%05d.zout' %(root, id))
-            print 'Refit, fine sampling: [%.2f, %.2f]' %(ztmp.l99[1], ztmp.u99[1])
             
             eazy_param = eazy.EazyParam('%s_%05d.eazy.param' %(root, id))
             eazy_param.params['TEMPLATES_FILE'] = TEMPLATES_FILE
-            eazy_param.params['Z_MIN'] = ztmp.l99[1]
-            eazy_param.params['Z_MAX'] = ztmp.u99[1]
+            eazy_param.params['Z_MIN'] = ztmp.l99[1]-0.15*(1+ztmp.z_peak[1])
+            eazy_param.params['Z_MAX'] = ztmp.u99[1]+0.15*(1+ztmp.z_peak[1])
             eazy_param.params['Z_STEP'] = 0.0025
             eazy_param.write(file='%s_%05d' %(root, id) + '.eazy.param')
+
+            print 'Refit, fine sampling: [%.2f, %.2f]' %(eazy_param.params['Z_MIN'], eazy_param.params['Z_MAX'])
             
         else:
             funicorn.analysis.make_eazy_inputs(root=root, id=id, OLD_RES = OLD_RES, bin_spec=bin_spec, spec_norm=spec_norm, zmin=zmin, zmax=zmax, zstep=0.0025, compress=compress, TILT_COEFFS=tilt, TEMPLATES_FILE=TEMPLATES_FILE)

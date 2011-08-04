d5b5e12540a46359cbc801856822f768128dd124
Gabriel Brammer <brammer@stsci.edu>
2011-08-04 18:02:16 +0000

    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@324 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- catalogs.py
+++ catalogs.py
@@ -911,8 +911,9 @@ def make_full_redshift_catalog():
     fp.writelines(spec_lines)
     fp.close()
     
-    status = os.system('cp *_redshift.cat /Library/WebServer/Documents/P/GRISM_v1.6/ANALYSIS')
-    status = os.system('gzip /Library/WebServer/Documents/P/GRISM_v1.6/ANALYSIS/*_redshift.cat')
+    if unicorn.hostname().startswith('uni'):
+        status = os.system('cp *_redshift.cat /Library/WebServer/Documents/P/GRISM_v1.6/ANALYSIS')
+        status = os.system('gzip /Library/WebServer/Documents/P/GRISM_v1.6/ANALYSIS/*_redshift.cat')
     
 def combine_matched_catalogs():
     """

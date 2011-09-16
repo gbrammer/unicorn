ea70a3150a47902662ccaecd43b151eaabd975ac
Gabriel Brammer <brammer@stsci.edu>
2011-09-16 01:44:07 +0000

    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@410 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- analysis.py
+++ analysis.py
@@ -173,7 +173,7 @@ def read_catalogs(root='', cosmos=False, aegis=False, goodsn=False, cdfs=False, 
         GRISM_PATH=unicorn.GRISM_HOME+'GOODS-N/'
         CAT_PATH = '/research/HST/GRISM/3DHST/GOODS-N/MODS/FAST/'
         if unicorn.hostname().startswith('uni'):
-            CAT_PATH = '/3DHST/Photometry/Release/GOODS-N/v1.7'
+            CAT_PATH = '/3DHST/Photometry/Release/GOODS-N/v1.7/'
         #
         CAT_FILE = CAT_PATH+'goodsn_v1.7.fullz.cat'
         ZOUT_FILE = CAT_PATH+'goodsn_v1.7_eazy/photz_v1.7.fullz.zout'

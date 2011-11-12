3d378bea3fe09e082bf2d203b783c389d96dc4fc
Gabriel Brammer <brammer@stsci.edu>
2011-11-12 02:53:33 +0000

    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@489 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- analysis.py
+++ analysis.py
@@ -1926,7 +1926,7 @@ def run_eazy_fit(root='COSMOS-23-G141', id=39, OLD_RES = 'FILTER.RES.v9.R300', O
         #### If fitting with photometry, first run with eazy line templates 
         #### and coarse sampling
         if unicorn.analysis.HAS_PHOTOMETRY:
-            unicorn.analysis.make_eazy_inputs(root=root, id=id, OLD_RES = OLD_RES, bin_spec=bin_spec, spec_norm=spec_norm, zmin=zmin, zmax=zmax, zstep=0.025, compress=1.5, TILT_COEFFS=tilt, TEMPLATES_FILE='templates/eazy_v1.1_lines_suppl.spectra.param', eazy_working_directory=eazy_working_directory)
+            unicorn.analysis.make_eazy_inputs(root=root, id=id, OLD_RES = OLD_RES, bin_spec=bin_spec, spec_norm=spec_norm, zmin=zmin, zmax=zmax, zstep=0.025, compress=2, TILT_COEFFS=tilt, TEMPLATES_FILE='templates/eazy_v1.1_lines_suppl.spectra.param', eazy_working_directory=eazy_working_directory)
 
             status = os.system(eazy_binary + ' -p '+'%s_%05d' %(root, id)+'.eazy.param '+pipe)
             
@@ -2003,6 +2003,7 @@ def run_eazy_fit(root='COSMOS-23-G141', id=39, OLD_RES = 'FILTER.RES.v9.R300', O
             eazy_param.write(file='%s_%05d' %(root, id) + '.eazy.param')
             #
             status = os.system(eazy_binary + ' -p '+'%s_%05d' %(root, id)+'.eazy.param '+pipe)
+            ztmp = catIO.Readfile('OUTPUT/%s_%05d.zout' %(root, id))
             
         if tilt is False:
             tilt = [0,1]

6dd23af9aa2d44b785384cdf2e463e36cdbb42ae
Gabriel Brammer <brammer@stsci.edu>
2011-11-12 04:15:16 +0000

    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@505 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- analysis.py
+++ analysis.py
@@ -1927,11 +1927,12 @@ def run_eazy_fit(root='COSMOS-23-G141', id=39, OLD_RES = 'FILTER.RES.v9.R300', O
         if unicorn.analysis.HAS_PHOTOMETRY:
             ### first run with eazy line templates 
             ### and coarse sampling, broaden the compression to hopefully catch a line
-            unicorn.analysis.make_eazy_inputs(root=root, id=id, OLD_RES = OLD_RES, bin_spec=bin_spec, spec_norm=spec_norm, zmin=zmin, zmax=zmax, zstep=0.025, compress=3, TILT_COEFFS=tilt, TEMPLATES_FILE='templates/eazy_v1.1_lines_suppl.spectra.param', eazy_working_directory=eazy_working_directory)
+            if not force_zrange:
+                unicorn.analysis.make_eazy_inputs(root=root, id=id, OLD_RES = OLD_RES, bin_spec=bin_spec, spec_norm=spec_norm, zmin=zmin, zmax=zmax, zstep=0.025, compress=3, TILT_COEFFS=tilt, TEMPLATES_FILE='templates/eazy_v1.1_lines_suppl.spectra.param', eazy_working_directory=eazy_working_directory)
             
             #os.system('grep Z_ %s_%05d.eazy.param' %(root, id))
-            status = os.system(eazy_binary + ' -p '+'%s_%05d' %(root, id)+'.eazy.param '+pipe)
-            ztmp = catIO.Readfile('OUTPUT/%s_%05d.zout' %(root, id))
+                status = os.system(eazy_binary + ' -p '+'%s_%05d' %(root, id)+'.eazy.param '+pipe)
+                ztmp = catIO.Readfile('OUTPUT/%s_%05d.zout' %(root, id))
 
             ### Remake the input files for the desired compression
             unicorn.analysis.make_eazy_inputs(root=root, id=id, OLD_RES = OLD_RES, bin_spec=bin_spec, spec_norm=spec_norm, zmin=zmin, zmax=zmax, zstep=0.001, compress=compress, TILT_COEFFS=tilt, TEMPLATES_FILE=TEMPLATES_FILE, eazy_working_directory=eazy_working_directory)

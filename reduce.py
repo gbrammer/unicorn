95d7b5fdb6db45636eb19149b7bbd02dbf5caaed
Gabriel Brammer <brammer@stsci.edu>
2012-03-09 02:48:08 +0000

    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@668 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- reduce.py
+++ reduce.py
@@ -2091,7 +2091,7 @@ def interlace_combine_blot(root='COSMOS-19-F140W', view=True, pad=60, REF_ROOT =
     NOBJ = len(old_cat.id)
     for i in range(NOBJ):
         #xw, yw = wcs_this.rd2xy((old_cat.ra[i], old_cat.dec[i]))
-        xw, yw = ref_wcs.wcs_sky2pix([[old_cat.ra[i], old_cat.dec[i]]],1)[0]
+        xw, yw = wcs_this.wcs_sky2pix([[old_cat.ra[i], old_cat.dec[i]]],1)[0]
         fp.write('%.3f %.3f\n' %(xw, yw))
     
     fp.close()

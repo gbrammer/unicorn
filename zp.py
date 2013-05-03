adbae35a11a8285de8233ccd7c5b28c48065035f
Gabriel Brammer <brammer@stsci.edu>
2013-05-03 21:10:24 +0000

    more testing of ZP scripts.
    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@892 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- zp.py
+++ zp.py
@@ -18,7 +18,10 @@ class IterZeropoints():
 
         ##### Read the param file
         self.param = eazy.EazyParam(PARAM_FILE=PATH+'/'+root+'.param')
-
+        self.filter_index = {}
+        for i in range(self.param.NFILT):
+            self.filter_index[self.param.filters[i].fnumber] = i
+            
         ##### Read template fluxes and coefficients
         self.tempfilt, coeffs, temp_seds, pz = eazy.readEazyBinary(MAIN_OUTPUT_FILE=root, OUTPUT_DIRECTORY=PATH,CACHE_FILE = 'Same')
         self.lc = self.tempfilt['lc']
@@ -65,6 +68,51 @@ class IterZeropoints():
         self.zeropoints = np.ones(self.phot.NFILT)
         self.first = 1
         self.prior_sigma = 0.01
+    
+    def go_fit_UDS_v3(self):
+        import unicorn.catalogs2 as cat2
+        
+        #### Make catalog with more spec-zs
+        zsp = cat2.SpeczCatalog()
+        cat = catIO.Readfile('uds_3dhst.v3.0.nzpcat', force_lowercase=False)
+        matcher = catIO.CoordinateMatcher(cat2.cat)
+        
+        cat2.read_catalogs('UDS')
+        dr, idx = matcher.match_list(cat.ra, cat.dec)
+
+        zsp3d = (cat2.dq.f_cover > 0.8) & (cat2.dq.f_negative < 0.1) & (cat2.cat.m140 < 22.5) & ((cat2.lines['HALPHA_FLUX']/cat2.lines['HALPHA_FLUX_ERR'] > 3) | (cat2.lines['OIII_FLUX']/cat2.lines['OIII_FLUX_ERR'] > 3))
+        ok = (dr < 0.5) & zsp3d[idx]
+        self.zout.z_spec[ok] = cat2.zfit.z_max_spec[idx][ok]*1
+        
+        dr, idx = zsp.match_list(cat.ra, cat.dec)
+        ok = (dr < 0.5) & (zsp.zspec[idx] > 0)
+        #self.zout.z_spec = self.zout.z_spec*0.-99        
+        self.zout.z_spec[ok] = zsp.zspec[idx][ok]*1
+                
+        cat.z_spec = self.zout.z_spec
+        cat.write_text(filename='uds_3dhst.v3.0.nzpcat.z_3d', select=cat.z_spec > 0)
+        
+        #### Initial perturbed bands
+        
+        self.initialize_arrays()
+        
+        self.first = np.ones(self.phot.NFILT)
+        self.prior_sigma = np.ones(self.phot.NFILT)*0.02
+        
+        ### u, subaru bands are a mess
+        initial_adjustments = [[88, 1.3, 0.1], [122, 0.95, 0.1], [79, 0.84, 0.05], [123, 0.8, 0.1], [124, 0.81, 0.1], [125, 0.9, 0.1], [236, 1.1, 0.2], [239, 1.1, 0.2]]
+        for adj in initial_adjustments:
+            ix = self.filter_index[adj[0]]
+            self.first[ix] = adj[1]
+            self.prior_sigma[ix] = adj[2]
+        #
+        self.fit_emcee(NWALKERS=50, NSTEP=100, verbose=True)
+        
+        for i in range(self.phot.NFILT):
+            FID = 'F%d' %(self.param.filters[i].fnumber)
+            norm_chain = self.chain[FID]/self.chain['F265']
+            print '%s  %.3f' %(FID, self.chain.get_stats(norm_chain[:, self.chain.nburn:], raw=True)['q50'])
+        
         
     def go_fit(self):
         import unicorn.catalogs2 as cat2
@@ -107,7 +155,7 @@ class IterZeropoints():
         
         colors = range(self.phot.NFILT)
         for ci, i in enumerate(np.argsort(self.lc)):
-            colors[i] = threedhst.utils.color_table((ci+1.)/tempfilt['NFILT']*250, table='rainbow.rgb')
+            colors[i] = threedhst.utils.color_table((ci+1.)/self.tempfilt['NFILT']*250, table='rainbow.rgb')
         
         for i in range(self.phot.NFILT):
             FID = 'F%d' %(self.param.filters[i].fnumber)
@@ -117,14 +165,14 @@ class IterZeropoints():
         #
         for i in range(self.phot.NFILT):
             FID = 'F%d' %(self.param.filters[i].fnumber)
-            norm_chain = self.chain[FID]/self.chain['F205']
+            norm_chain = self.chain[FID]/self.chain['F265']
             print '%s  %.3f' %(FID, self.chain.get_stats(norm_chain[:, self.chain.nburn:], raw=True)['q50'])
             
     def fit_emcee(self, NWALKERS=50, NSTEP=200, verbose=True):
         import time
         import emcee
         
-        p0 = np.array([np.random.normal(size=self.phot.NFILT)*self.prior_sigma+self.first for i in range(NWALKERS)])
+        p0 = np.array([np.random.normal(size=self.phot.NFILT)*0.05+self.first for i in range(NWALKERS)])
         
         obj_fun = self._objective_loop_over_objects
         obj_args = [self] ## 
@@ -158,6 +206,9 @@ class IterZeropoints():
         model = np.zeros((self.NOBJ, self.phot.NFILT))
         for i in range(self.NOBJ):
             m_i = self.mask[i,:]
+            if m_i.sum() == 0:
+                continue
+            #
             t_i = self.templates[i,:,:]
             ### Warning: updating coefficients in place
             self.coeffs[i,:]  = utils_c.run_nmf(tweaked[i,m_i], self.full_var[i,m_i], t_i[:,m_i], self.amatrix[i,:,:], toler=1.e-4, MAXITER=1e6, init_coeffs=self.coeffs[i,:] )

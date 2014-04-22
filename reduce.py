00991c28acca10468891e63f74f40a573774ec36
Gabriel Brammer <brammer@stsci.edu>
2014-04-22 13:28:49 +0000

    Handle filters that don't start with "F1" (e.g., F098M) in `interlace_combine_blot`.
    git-svn-id: https://subversion.assembla.com/svn/threedhst_internal/trunk@1014 f9184c78-529c-4a83-b317-4cf1064cc5e0

--- reduce.py
+++ reduce.py
@@ -2994,8 +2994,9 @@ def interlace_combine_blot(root='COSMOS-19-F140W', view=True, pad=60, REF_ROOT =
     
     if unicorn.hostname().startswith('uni'):
         view = False
-        
-    pointing = root.split('-F1')[0]
+    
+    ### Strip off filter name    
+    pointing = '-F'.join(root.split('-F')[:-1])
     
     #unicorn.reduce.blot_from_reference(REF_ROOT=REF_ROOT, DRZ_ROOT = root, NGROW=NGROW, verbose=verbose)
     
